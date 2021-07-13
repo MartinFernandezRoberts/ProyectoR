4848094bd00488d904c9372ea4659afda5d98dce
matiac <ac.matif@gmail.com>
2021-07-13 18:23:56 -0400

    casas destacadas

---
+++ src/index/components/svg/FlechitaIcon.vue
@@ -0,0 +1,17 @@
+<template>
+    <svg
+        viewBox="0 0 10 20"
+        stroke="currentColor"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        fill="none"
+    >
+        <polyline points="9,1 1,10 9,19" />
+    </svg>
+</template>
+
+<script>
+export default {
+    name: 'FlechitaIcon',
+};
+</script>
