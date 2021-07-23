604475bb208234c8bc1fc38a5fe46c8ee82c51ca
UlfhednarKutral <martinfrobertsweb@gmail.com>
2021-07-23 13:07:57 -0400

    modelo casa

--- server/models/Casa.js
+++ server/models/Casa.js
@@ -37,6 +37,85 @@ const CasaSchema = new mongoose.Schema({
             required: true,
         },
     ],
+    area: {
+        type: Number,
+    },
+    wc: {
+        type: Number,
+    },
+    orientacion: {
+        type: String,
+        default: '',
+        enum: ['Oriente', 'Poniente','Norte','Sur', 'Nororiente', 'Norponiente','Suroriente', 'Surponiente'],
+    },
+    estacionamiento: {
+        type: Number
+    },
+    dormitorio: {
+        type: Number,
+    },
+    piso: {
+        type: Number,
+    },
+    bodega: {
+        type: Boolean,
+        default: false
+    },
+    mascotas: {
+        type: Boolean,
+        default: false
+    },
+    calefaccion: {
+        type: Boolean,
+        default: false
+    },
+    cocina: {
+        type: Boolean,
+        default: false
+    },
+    terraza: {
+        type: Boolean,
+        default: false
+    },
+    balcon: {
+        type: Boolean,
+        default: false
+    },
+    piscina: {
+        type: Boolean,
+        default: false
+    },
+    quincho: {
+        type: Boolean,
+        default: false
+    },
+    gastosComunes: {
+        type: Number,
+    },
+    piscinaE: {
+        type: Boolean,
+        default: false
+    },
+    gimnasioE: {
+        type: Boolean,
+        default: false
+    },
+    lavanderiaE: {
+        type: Boolean,
+        default: false
+    },
+    eventosE: {
+        type: Boolean,
+        default: false
+    },
+    quinchoE: {
+        type: Boolean,
+        default: false
+    },
+    terraza: {
+        type: Boolean,
+        default: false
+    },
 });
 
 module.exports = mongoose.model('Casa', CasaSchema);
