f0f7b0b99d5e1a4bac985ad5e928c13cd3a39f1e
matiac <ac.matif@gmail.com>
2021-08-13 13:51:18 -0400

    slide

--- src/index/components/landing/ComoParticipar.vue
+++ src/index/components/landing/ComoParticipar.vue
@@ -2,13 +2,21 @@
     <section
         id="como-funciona"
         class="flex justify-center items-center bg-green-400"
+        ref="como"
     >
         <h2 class="text-4xl text-white font-bold">Aquí dirá cómo participar</h2>
+
+        <button type="button" @click="scrollear">scroll</button>
     </section>
 </template>
 
 <script>
 export default {
     name: 'ComoParticipar',
+    methods: {
+        scrollear() {
+            this.$refs.como.scrollIntoView();
+        },
+    },
 };
 </script>
