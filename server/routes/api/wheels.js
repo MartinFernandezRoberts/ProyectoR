3f4094158f2060e592ee4aef55837acb20f7d986
UlfhednarKutral <martinfrobertsweb@gmail.com>
2021-08-12 20:33:28 -0400

    wheels updated

--- server/routes/api/wheels.js
+++ server/routes/api/wheels.js
@@ -5,22 +5,27 @@ const path = require('path');
 /* const mongodb = require('mongodb')
 const connectDB = require('../../config/db')
 const { ensureAuth } = require('../../middleware/auth') */
+const Item = require('../../models/Item');
 const Wheels = require('../../models/Wheels');
-const Destacado = require('../../models/Destacado');
 
 const ImageUploader = require('./ImageUploader');
-const imgUp = new ImageUploader('wheels');
+const imgUp = new ImageUploader('wheel');
 
 // @desc api/Private page
-// @route GET /panel/api/wheels
+// @route GET /staff/api/wheel
 
 //GET
 router.get('/', async (req, res) => {
     try {
-        const wheels = await Wheels.find().lean();
-        res.send({
-            wheels,
-        });
+        const wheels = await Item.find({
+            tipo: 'Wheels',
+            estado: { $ne: 'bajado' },
+        })
+            .sort('-fecha')
+            .populate('item')
+            .lean();
+
+        res.send(wheels);
     } catch (err) {
         console.error(err);
         res.status(500).send(err);
@@ -28,17 +33,24 @@ router.get('/', async (req, res) => {
 });
 
 // @desc api/Private page
-// @route POST /panel/api/wheels
+// @route POST /panel/api/wheel
 router.post('/', imgUp.upload.array('files', 10), async (req, res) => {
     try {
-        const rutasImagenes = req.files.map(
-            (file) => 'img/wheels/' + file.filename
-        );
-
         console.log(req.body);
-        await Wheels.create({ ...req.body, imagen: rutasImagenes });
 
-        res.status(201).send('Registro Agregado');
+        const wheel = await Wheels.create(req.body.wheel);
+
+        const rutasImagenes = req.files.map(
+            (file) => 'img/wheel/' + file.filename
+        );
+        await Item.create({
+            tipo: 'Wheels',
+            item: wheel._id,
+            ...req.body.item,
+            imagenes: rutasImagenes,
+        });
+
+        res.status(201).send('Wheels agregada');
     } catch (err) {
         console.error(err);
         res.status(500).send(err);
@@ -46,43 +58,82 @@ router.post('/', imgUp.upload.array('files', 10), async (req, res) => {
 });
 
 // @desc api/Update
-// @route PUT /panel/api/wheels
-router.put('/editar/:id', async (req, res) => {
-    try {
-        await Wheels.findOneAndUpdate(
-            {
-                _id: req.params.id,
-            },
-            {
-                $set: {
-                    titulo: req.body.titulo,
-                    descripcion: req.body.descripcion,
-                    ubicacion: req.body.ubicacion,
-                    estado: req.body.estado,
-                    fecha: req.body.fecha,
-                },
-            },
-            {
-                upsert: true,
+// @route PUT /panel/api/wheel
+router.post(
+    '/:id/editar',
+    imgUp.upload.array('files', 10),
+    async (req, res) => {
+        try {
+            console.log(req.body);
+            let paBorrar = req.body.paBorrar || [];
+            if (!Array.isArray(paBorrar)) paBorrar = [paBorrar];
+            const item = await Item.findById(req.params.id).exec();
+            let imagenes = item.get('imagenes');
+            imagenes = imagenes.filter((imagen) => !paBorrar.includes(imagen));
+
+            const rutasImagenes = req.files.map(
+                (file) => 'img/wheel/' + file.filename
+            );
+
+            imagenes = imagenes.concat(rutasImagenes);
+
+            // aquí se podría definir la destacada.
+
+            await item.update({ ...req.body.item, imagenes });
+            await Wheels.updateOne(
+                { _id: item.item },
+                { ...req.body.wheel, imagenes }
+            );
+
+            if (paBorrar.length > 0) {
+                console.log(
+                    'no hay pa borrar pero salgo igual y el if no anda'
+                );
+                paBorrar.forEach((imagen) => {
+                    const rutaImagen = path.join(
+                        __dirname,
+                        '../../public/',
+                        imagen
+                    );
+
+                    fs.unlink(rutaImagen, (err) => {
+                        if (err) console.error(err);
+                        console.log(`archivo eliminado: ${imagen}`);
+                    });
+                });
             }
+
+            res.status(201).send('Wheels actualizada');
+        } catch (err) {
+            console.error(err);
+            res.status(500).send(err);
+        }
+    }
+);
+
+router.post('/:id/destacar', async (req, res) => {
+    console.log(req.body);
+
+    try {
+        await Item.updateOne(
+            { _id: req.params.id },
+            { destacado: req.body.destacado }
         );
-        res.status(201).send('Registro Actualizado');
+        res.status(201).send('Wheels actualizada');
     } catch (err) {
         console.error(err);
         res.status(500).send(err);
     }
 });
+
 //delete
 router.delete('/:id', async (req, res) => {
     try {
-        await Destacado.deleteOne({
-            itemDestacado: req.params.id,
-        });
-        const wheels = await Wheels.findOneAndDelete({
+        const wheel = await Wheels.findOneAndDelete({
             _id: req.params.id,
         });
 
-        wheels.imagenCasa.forEach((imagen) => {
+        wheel.imagen.forEach((imagen) => {
             const rutaImagen = path.join(__dirname, '../../public/', imagen);
 
             fs.unlink(rutaImagen, (err) => {
